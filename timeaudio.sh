dd672014f5c963dc7666ac7dc9c945fc964f9e3c
Michael.Tian <michael.tian@loweprofero.com>
2017-08-10 13:37:21 +0800

    storage voice and script file.

---
+++ timeaudio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filePath=/home/pi/timeaudio
+#time=`date | awk -F \  {'print $4'} | awk -F : {'print $1'}`
+time=`date +%H%M`
+audioFile="$filePath/$time.mp3"
+echo $audioFile
+player=/usr/bin/mplayer
+
+$player $audioFile
+#$player >> /tmp/1.log
+#echo $audioFile >> /tmp/1.log
+
